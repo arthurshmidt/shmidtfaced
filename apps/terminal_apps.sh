417598978efdd2a30ca78ae637c3cd98364e1694
arthurshmidt <arthur@arthurshmidt.com>
2025-08-26 13:10:06 -0400

    Updated Terminal Application install loop

--- apps/terminal_apps.sh
+++ apps/terminal_apps.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-for application in $(cat terminal_apps.txt); do
-	echo "$application"
-done
+# for application in $(cat terminal_apps.txt); do
+# 	echo "$application"
+# done
+
+while read -r line; do
+#	echo "This is the line: $line"
+	read -r application _ <<< "$line"
+#	echo "this is the application: $application"
+	echo -e "\e[32mInstalling... $application\e[0m"
+	sudo apt -y install "$application"
+done < terminal_apps.txt
